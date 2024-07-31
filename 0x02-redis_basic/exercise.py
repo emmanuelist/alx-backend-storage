f556cae1d32cd22c615aefab43b56c363b0b710a
emmanuelist <emmanuel.paul75@yahoo.com>
2024-07-31 19:25:38 +0100

    feat(cache): Implement Redis cache management class with data storage and retrieval methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/bin/usr/bash python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
@@ -19,13 +19,6 @@ class Cache:
 
     store(data: Union[str, bytes, int, float]) -> str
         Stores the given data in the cache and returns a unique key for the stored data.
-
-    Examples
-    --------
-    >>> cache = Cache()
-    >>> key = cache.store("Hello, World!")
-    >>> print(key)
-    'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
     """
 
     def __init__(self):
@@ -52,16 +45,60 @@ class Cache:
         -------
         str
             A unique key for the stored data.
-
-        Examples
-        --------
-        >>> cache = Cache()
-        >>> key1 = cache.store("Hello, World!")
-        >>> key2 = cache.store(12345)
-        >>> key3 = cache.store(b"Hello, World!")
-        >>> print(key1, key2, key3)
-        'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves the data associated with the given key from the cache and applies an optional transformation function.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to be retrieved from the cache.
+        fn : Callable, optional
+            An optional transformation function to be applied to the retrieved data (default is None).
+
+        Returns
+        -------
+        Union[str, bytes, int, float]
+            The retrieved data, possibly transformed by the given function.
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves the data associated with the given key from the cache and returns it as a string.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to be retrieved from the cache.
+
+        Returns
+        -------
+        str
+            The retrieved data as a string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves the data associated with the given key from the cache and returns it as an integer.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to be retrieved from the cache.
+
+        Returns
+        -------
+        int
+            The retrieved data as an integer.
+        """
+        return self.get(key, fn=int)
