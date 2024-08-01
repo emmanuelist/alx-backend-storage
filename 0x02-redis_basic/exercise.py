656095825fa9092335500deae049103a0ca983ea
emmanuelist <emmanuel.paul75@yahoo.com>
2024-08-01 10:21:10 +0100

    "Added call_history decorator to store method call inputs and outputs in Redis, and applied it to the store method. Reformatted code with consistent line breaks and indentation."

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@ import uuid
 import functools
 from typing import Union, Callable
 
+
 def count_calls(method: Callable) -> Callable:
     """
     A decorator to count the number of calls to a method.
@@ -21,6 +22,29 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """
+    A decorator to store the call history of a method.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
 class Cache:
     """
     A class used to manage data in a Redis cache.
@@ -33,32 +57,39 @@ class Cache:
     Methods
     -------
     __init__()
-        Initializes the Cache instance by creating a Redis client and flushing the database.
+        Initializes the Cache instance by creating a Redis
+        client and flushing the database.
 
     store(data: Union[str, bytes, int, float]) -> str
-        Stores the given data in the cache and returns a unique key for the stored data.
+        Stores the given data in the cache and returns a
+        unique key for the stored data.
     """
 
     def __init__(self):
         """
-        Initializes the Cache instance by creating a Redis client and flushing the database.
+        Initializes the Cache instance by creating a
+        Redis client and flushing the database.
 
         Note
         ----
-        This will delete all existing data in the Redis database. Use with caution.
+        This will delete all existing data in the Redis database.
+        Use with caution.
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Stores the given data in the cache and returns a unique key for the stored data.
+        Stores the given data in the cache and returns
+        a unique key for the stored data.
 
         Parameters
         ----------
         data : Union[str, bytes, int, float]
-            The data to be stored in the cache. It can be of type str, bytes, int, or float.
+            The data to be stored in the cache.
+            It can be of type str, bytes, int, or float.
 
         Returns
         -------
@@ -69,16 +100,23 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self,
+            key: str,
+            fn: Callable = None) -> Union[str,
+                                          bytes,
+                                          int,
+                                          float]:
         """
-        Retrieves the data associated with the given key from the cache and applies an optional transformation function.
+        Retrieves the data associated with the given key from the cache
+        and applies an optional transformation function.
 
         Parameters
         ----------
         key : str
             The key of the data to be retrieved from the cache.
         fn : Callable, optional
-            An optional transformation function to be applied to the retrieved data (default is None).
+            An optional transformation function to be applied to
+            the retrieved data (default is None).
 
         Returns
         -------
@@ -92,7 +130,8 @@ class Cache:
 
     def get_str(self, key: str) -> str:
         """
-        Retrieves the data associated with the given key from the cache and returns it as a string.
+        Retrieves the data associated with the given key
+        from the cache and returns it as a string.
 
         Parameters
         ----------
@@ -108,7 +147,8 @@ class Cache:
 
     def get_int(self, key: str) -> int:
         """
-        Retrieves the data associated with the given key from the cache and returns it as an integer.
+        Retrieves the data associated with the given key from
+        the cache and returns it as an integer.
 
         Parameters
         ----------
