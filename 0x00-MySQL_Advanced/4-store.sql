ad0691062cc2cfa564e1737661aa382347710159
emmanuelist <emmanuel.paul75@yahoo.com>
2024-08-08 15:28:31 +0100

    feat: Add SQL scripts for initializing and manipulating items and orders tables

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+CREATE TRIGGER decrease_q
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - new.number
+WHERE name = new.item_name;
